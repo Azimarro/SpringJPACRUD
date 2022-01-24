4db42fbc2d3ad209a5a773abbdd801fb7f903907
Azim Isroilov <isro.azim@gmail.com>
2022-01-24 16:05:35 +0500

    detete test classes

--- src/main/java/com/example/JpaCRUD/test/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.JpaCRUD.test;
-//for test git branches, you also can delete this class and package
-public class Test {
-
-    private int id;
-    private String name;
-}
--- src/main/java/com/example/JpaCRUD/test/Test2.java
+++
@@ -1,4 +0,0 @@
-package com.example.JpaCRUD.test;
-
-public class Test2 {
-}
